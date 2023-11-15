f34c6a6699e9d585d3acc800ec09e54564e4dbfb
Silu Hu <erinh816@gmail.com>
2023-11-15 17:22:52 -0500

    add all routes for listings

--- routes/listings.js
+++ routes/listings.js
@@ -4,6 +4,7 @@ const express = require("express");
 const { BadRequestError, NotFoundError } = require("../expressError");
 const Listing = require("../models/Listing");
 const User = require("../models/User");
+const Booking = require("../models/Booking");
 
 // ***************************************************************** S3 AWS
 const BUCKET_NAME = process.env.BUCKET_NAME;
@@ -30,7 +31,7 @@ const router = new express.Router();
 
 /**TODO: */
 router.get("/", async function (req, res, next) {
-  const listings = await Listing.findAll({include: User});
+  const listings = await Listing.findAll({ include: User });
 
   //Use if host should be included in listing
   // const listings = await Listing.findAll({include: User});
@@ -39,19 +40,19 @@ router.get("/", async function (req, res, next) {
 });
 
 /**TODO: */
-router.get("/:id", async function (req, res, next){
+router.get("/:id", async function (req, res, next) {
   const params = req.params.id;
 
   const listing = await Listing.findOne({
     where: {
       id: Number(params)
     }
-  })
+  });
 
-  return res.json({listing});
-})
+  return res.json({ listing });
+});
 
-router.post("/add", upload.single("image"), async function(req, res, next){
+router.post("/add", upload.single("image"), async function (req, res, next) {
   req.file.buffer;
 
   const params = {
@@ -67,16 +68,16 @@ router.post("/add", upload.single("image"), async function(req, res, next){
   //TODO: implement saving image url to database
   console.log(`https://${BUCKET_NAME}.s3.${BUCKET_REGION}.amazonaws.com/${req.file.originalname}`);
 
-  const {title,
+  const { title,
     description,
     address,
     city,
     state,
     zipcode,
     price_per_day,
-    host_id} = req.body;
+    host_id } = req.body;
 
-    console.log('Body', req.body)
+  console.log('Body', req.body);
 
   const listing = await Listing.create({
     title,
@@ -87,12 +88,35 @@ router.post("/add", upload.single("image"), async function(req, res, next){
     zipcode,
     price_per_day,
     host_id,
-    image:`https://${BUCKET_NAME}.s3.${BUCKET_REGION}.amazonaws.com/${req.file.originalname}`
-  })
+    image: `https://${BUCKET_NAME}.s3.${BUCKET_REGION}.amazonaws.com/${req.file.originalname}`
+  });
 
-  res.json({listing});
-})
+  res.status(201).json({ listing });
+});
+
+//TODO: doc
+router.patch("/:id", async function (req, res, next) {
+
+  const listing = await Listing.findByPk(req.params.id);
+
+  await listing.update(req.body);
+  await listing.save();
+
+  return res.json({ listing });
+  //TODO: could add update image
+});
 
 
+//TODO:doc
+router.delete("/:id", async function (req, res, next) {
+  const listing = await Listing.findByPk(req.params.id);
+  await Booking.destroy({ where: { listing_id: req.params.id } });
+
+  await listing.destroy();
+
+  return res.json({ deleted: `${req.params.id} is deleted!` });
+});
+
+//TODO:error handling
 
 module.exports = router;
